761fcf18e0cf899e65d28a61c5d2df969bf1b283
mead-d <meadd@my.erau.edu>
2021-12-06 14:16:45 -0500

    adds comments for legibility

--- SwarmScripts/StackList.py
+++ SwarmScripts/StackList.py
@@ -1,11 +1,14 @@
 # Written by Dillon Mead
 
+# Doubly linked list implemented as stack. Top of stack is list tail
 class StackList:
     size = 0
 
+    # constructor
     def __init__(self):
         self.head = self.tail = None
 
+    # adds node with input as value to tail. increments size by one
     def push(self, val):
         if self.head == self.tail == None:
             self.head = Node(val)
@@ -14,9 +17,10 @@ class StackList:
         else:
             self.tail.next = Node(val)
             self.tail.next.prev = self.tail
-            self.tail.next = self.tail
+            self.tail = self.tail.next
             self.size += 1
 
+    # removes top(tail) of stack. decrements size by one
     def pop(self):
         item = self.data
         self.prev.next = None
@@ -25,20 +29,25 @@ class StackList:
         self.size -= 1
 
         return item
-        
+    
+    # returns the value of the top node
     def top(self):
         return self.tail.data
 
+    # returns the recorded size
     def getSize(self):
         return self.size
 
+    # prints stack from head to tail
     def display(self):
         cur = self.head
         while cur is not None:
             print(cur.val, end = ' ')
             cur = cur.next
             
+
 class Node:
+    # constructor
     def __init__(self, data):
         self.data = data
         self.next = None
