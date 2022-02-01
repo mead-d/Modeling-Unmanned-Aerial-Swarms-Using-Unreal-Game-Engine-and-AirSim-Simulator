f6eb9d70fc1df8dab2db27207d6a7c63d6120d3c
mead-d <meadd@my.erau.edu>
2022-02-01 14:14:39 -0500

    Updates distance3D for more general use

--- SwarmScripts/distance3D.py
+++ SwarmScripts/distance3D.py
@@ -3,19 +3,15 @@ import math
 
 class distance3D:
 
-    # Something, something, something Dark Side.
-    # Something, something, something complete.
-    def calcDistance(coordinate1, coordinate2):
-        
-        # Split coordinates into dimensional components
-        c1_x = coordinate1.x_val
-        c1_y = coordinate1.y_val
-        c1_z = coordinate1.z_val
+    # @Param: {x,y,x} coordinate set 1 and coordinate set 2.
+    # @Return: 3 Dimensional distance between coordinate pair.
+    def calcDistance(x1, y1, z1, x2, y2, z2):
 
-        c2_x = coordinate2.x_val
-        c2_y = coordinate2.y_val
-        c2_z = coordinate2.z_val
-        
         # 3D distance between two coordinates
-        return math.sqrt((c2_x - c1_x)**2 + (c2_y - c1_y)**2 + (c2_z - c1_z)**2)
+        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
 
+    # Function to separate Lidar coordinates into single dimension variables
+
+
+    # Function to separate drone kinematic coordinates into single dimension variables
+    
