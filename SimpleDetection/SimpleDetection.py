d24f8b00fd57cbe83ed829b23371c194d8e83efd
johnmueller909 <63922117+johnmueller909@users.noreply.github.com>
2022-02-03 16:41:54 -0500

    Added Simple Detection Files
    Includes simpleDetection.py and the settings.json files. When simpleDetection is called, it will look at the current lidar data that is being gathered and if something is in the lidar's range (currently 30 meters), it will return true, prompting the collision avoidance algorithm to act. Settings.json may have to be tweaked (mainly just the horizontal and vertical settings)

---
+++ SimpleDetection/SimpleDetection.py
@@ -0,0 +1,64 @@
+# Purpose: Detect Objects Utilizing Lidar
+
+# Created by John Mueller
+
+import setup_path 
+import airsim
+
+import sys
+import math
+import time
+import argparse
+import pprint
+import numpy
+
+class simpleDetection:
+
+#Used for testing
+ def __init__(self):
+
+        # connect to the AirSim simulator
+        self.client = airsim.MultirotorClient()
+        self.client.confirmConnection()
+        self.client.enableApiControl(True)
+
+ def execute(self):
+      
+        objectDetected = False 
+
+        #while objectDetected == False:
+
+        lidarData = self.client.getLidarData();
+        if (len(lidarData.point_cloud) < 3):
+           print("\tNo points received from Lidar data")
+        else:
+            print("\tObject Detected!")
+            objectDetected = True
+                
+        return objectDetected
+
+
+#Used for testing
+ def stop(self):
+
+    airsim.wait_key('Press any key to reset to original state')
+
+    self.client.armDisarm(False)
+    self.client.reset()
+
+    self.client.enableApiControl(False)
+    print("Done!\n")
+
+# main used for testing
+if __name__ == "__main__":
+    args = sys.argv
+    args.pop(0)
+ 
+    #SD = simpleDetection()
+    #try:
+    #    SD.execute()
+    #finally:
+    #    SD.stop()
+
+
+
