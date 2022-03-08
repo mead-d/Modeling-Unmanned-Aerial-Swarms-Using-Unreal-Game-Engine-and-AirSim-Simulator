c8a293cc7c5c885c5b4021d1abeaf1a154b70be6
johnmueller909 <63922117+johnmueller909@users.noreply.github.com>
2022-03-08 11:39:58 -0500

    Updated simpleDetection.py

--- SimpleDetection/SimpleDetection.py
+++ SimpleDetection/SimpleDetection.py
@@ -2,7 +2,7 @@
 
 # Created by John Mueller
 
-import setup_path 
+import setup_path
 import airsim
 
 import sys
@@ -14,49 +14,78 @@ import numpy
 
 class simpleDetection:
 
-#Used for testing
- #def __init__(self):
+    #used for testing
+    def __init__(self):
 
         # connect to the AirSim simulator
-        #self.client = airsim.MultirotorClient()
-        #self.client.confirmConnection()
-        #self.client.enableApiControl(True)
+        self.client = airsim.MultirotorClient()
+        self.client.confirmConnection()
+        self.client.enableApiControl(True)
 
- def detectObject(self,client):
-      
-        objectDetected = False 
+    def execute(self):
+    ##These lines are used for testing...
+        #print("arming the drone...")
+        #self.client.armDisarm(True)
+        #state = self.client.getMultirotorState()
+        #s = pprint.pformat(state)
+        #print("state: %s" % s)
+        #airsim.wait_key('Press any key to takeoff')
+        #self.client.takeoffAsync().join()
+        #state = self.client.getMultirotorState()
+        #print("state: %s" % pprint.pformat(state))
+        #airsim.wait_key('Press any key to move vehicle to (-10, 10, -10) at 5 m/s')
+        #self.client.moveToPositionAsync(10, 10, -10, 5).join()
+        #self.client.hoverAsync().join()
+        #airsim.wait_key('Press any key to get Lidar readings')
+       
+        for i in range(1,5):
+            lidarData = self.client.getLidarData();
+            if (len(lidarData.point_cloud) < 3):
+                print("\tNo points received from Lidar data")
+            else:
+                points = self.parse_lidarData(lidarData)
+                print("\tDanger, possible collsion detected")
+                print("\Time_stamp: %d number_of_points: %d" % (lidarData.time_stamp, len(points)))
+                #print("\t\tlidar position: %s" % (pprint.pformat(lidarData.pose.position)))
+                #print("\t\tlidar orientation: %s" % (pprint.pformat(lidarData.pose.orientation)))
+                print("\t\t PointCloud Data: %s" % (pprint.pformat(points)))
+                #return points
+           
+            return points
 
-        #while objectDetected == False:
+    def parse_lidarData(self, data):
 
-        lidarData = client.getLidarData()
-        if (len(lidarData.point_cloud) >= 3):
-            print("\tObject Detected!")
-            objectDetected = True
-                
-        return objectDetected
+        # reshape array of floats to array of [X,Y,Z]
+        points = numpy.array(data.point_cloud, dtype=numpy.dtype('f4'))
+        points = numpy.reshape(points, (int(points.shape[0]/3), 3))
+       
+        return points
 
+    def stop(self):
 
-#Used for testing
- def stop(self):
+        airsim.wait_key('Press any key to reset to original state')
 
-    airsim.wait_key('Press any key to reset to original state')
+        self.client.armDisarm(False)
+        self.client.reset()
 
-    self.client.armDisarm(False)
-    self.client.reset()
-
-    self.client.enableApiControl(False)
-    print("Done!\n")
+        self.client.enableApiControl(False)
+        print("Done!\n")
 
 # main used for testing
 if __name__ == "__main__":
     args = sys.argv
     args.pop(0)
+
+    arg_parser = argparse.ArgumentParser("Lidar.py makes drone fly and gets Lidar data")
+
+    arg_parser.add_argument('-save-to-disk', type=bool, help="save Lidar data to disk", default=False)
  
+    args = arg_parser.parse_args(args)    
+
     #SD = simpleDetection()
     #try:
-    #    SD.execute()
+        #SD.execute()
     #finally:
-    #    SD.stop()
-
+        #SD.stop()
 
 
