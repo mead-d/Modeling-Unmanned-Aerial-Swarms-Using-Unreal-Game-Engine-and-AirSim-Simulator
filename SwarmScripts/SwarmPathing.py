c52c759195eaeeb17b2b82ecd1fbf00b7523a899
Elijah Keck <73807523+ElijahKeck@users.noreply.github.com>
2022-03-08 14:00:20 -0500

    Adds method to turn and check  in direction of movement

--- SwarmScripts/SwarmPathing.py
+++ SwarmScripts/SwarmPathing.py
@@ -1,5 +1,6 @@
 import airsim
 import WaypointList as wpl
+import math
 
 
 
@@ -17,9 +18,11 @@ class SwarmPathing(object):
 
         vect = v3d.vectorize(drone_xpos,drone_ypos,drone_zpos,coords[0],coords[1],coords[2])
         ang = v3d.vectorAngle(vect[0],vect[1])
-
-        client.rotateToYawAsync(ang)
-        client.moveToPositionAsync(coords[0], coords[1], coords[2], speed, vehicle_name= drone)
+        velAng = v3d.vectorAngle(velocity[0],velocity[1])
+        if (ang < (velAng - math.pi/90) or ang > (velAng + math.pi/90)):
+            print("Off Course")
+            client.rotateToYawAsync(ang).join()
+            client.moveToPositionAsync(coords[0], coords[1], coords[2], speed, vehicle_name= drone)
 
     def pathCheck(self,wpl,drone,client):
         vNode = wpl.validWayPoint()
