cb88e4cc38c0d4f92c6e38e1eb86c15ff3b1aff9
mead-d <meadd@my.erau.edu>
2022-03-09 11:28:01 -0500

    de-couples vector calculations from AvoidanceAlg
    Extracted obstacle vector calculation and Right of Way conditions from AvoidanceAlg and into CollisionDetection

--- SwarmScripts/AvoidanceAlg.py
+++ SwarmScripts/AvoidanceAlg.py
@@ -1,7 +1,7 @@
 # Written by Dillon Mead
 import math
 import numpy
-import airsim
+#import airsim
 import WaypointList
 import Vector3D
 
@@ -10,48 +10,48 @@ class AvoidanceAlg:
     # UAV avoids by right turn with set radius. Includes FAA Right-of-Way measures
     # @Param: waypointlist of acting drone, the drone object, obstacle to avoid as array of coordinates, and client object
     # @Return: void
-    def rightTurnAvoid(self, wpl, drone, obstacle, client):
+    def rightTurnAvoid(self, wpl, drone):
     
         # vector3D module constructor
-        v3D = Vector3D.Vector3D()
+        #v3D = Vector3D.Vector3D()
 
         radius = 5 # constraint for vector distance
         new_speed = 5 # drone velocity during avoidance
 
         # determine drone position/ heading from AirSim client
-        droneState = client.getMultirotorState(vehicle_name = drone).kinematics_estimated
-        velocity = droneState.linear_velocity.to_numpy_array()   
-        drone_ypos = droneState.position.y_val
-        drone_xpos = droneState.position.x_val
-        drone_zpos = droneState.position.z_val
+        #droneState = client.getMultirotorState(vehicle_name = drone).kinematics_estimated
+        #velocity = droneState.linear_velocity.to_numpy_array()   
+        #drone_ypos = droneState.position.y_val
+        #drone_xpos = droneState.position.x_val
+        #drone_zpos = droneState.position.z_val
         
         # vector components from UAV to obstacle
-        objVector = v3D.vectorize(drone_xpos, drone_ypos, drone_zpos, obstacle[0], obstacle[1], obstacle[2])
+        #objVector = v3D.vectorize(drone_xpos, drone_ypos, drone_zpos, obstacle[0], obstacle[1], obstacle[2])
 
         # boolean condition if x xor y component is negative
         # negVector = objVector[0] < 0 != objVector[1] < 0
 
         # check if obstacle is converging from the left, with a bias of 5 degrees to the left.
-        if (objVector[0] < 0 != objVector[1] < 0) and (v3D.vectorAngle(objVector[0], objVector[1]) > 95 * math.pi / 180):
+        #if (objVector[0] < 0 != objVector[1] < 0) and (v3D.vectorAngle(objVector[0], objVector[1]) > 95 * math.pi / 180):
             # don't avoid for convergence from left
-            dummy = 0 # dummy var. python is dumb and won't recognize "do nothing" statement
+            #dummy = 0 # dummy var. python is dumb and won't recognize "do nothing" statement
+        #else:
+        # Calculate orthagonal vector. Avoids arithmatic error
+        if drone[3] == 0:                                    # case if x component is zero
+            orthagonal_heading = -1 * drone[4]              
+        elif drone[4] == 0:                                  # case if y component is zero
+            orthagonal_heading = drone[3]
         else:
-            # Calculate orthagonal vector. Avoids arithmatic error
-            if velocity[0] == 0:                                    # case if x component is zero
-                orthagonal_heading = -1 * velocity[1]              
-            elif velocity[1] == 0:                                  # case if y component is zero
-                orthagonal_heading = velocity[0]
-            else:
-                orthagonal_heading = -1 * velocity[0]/velocity[1]
+            orthagonal_heading = -1 * drone[3]/drone[4]
 
-            # find x, y coordinate ahead of drone constrained with designated radius
-            intercept_x = 2 * radius * math.sin(math.asin(velocity[0]/radius)) + drone_xpos
-            intercept_y = 2 * radius * math.cos(math.acos(velocity[1]/radius)) + drone_ypos
+        # find x, y coordinate ahead of drone constrained with designated radius
+        intercept_x = 2 * radius * math.sin(math.asin(drone[3]/radius)) + drone[0]
+        intercept_y = 2 * radius * math.cos(math.acos(drone[4]/radius)) + drone[1]
         
-            # find right turn waypoint with orthagonal vector constrained with designated radius
-            orthagonal_x = radius * math.sin(math.atan(orthagonal_heading)) + intercept_x
-            orthagonal_y = orthagonal_heading * (orthagonal_x - intercept_x) + intercept_y
+        # find right turn waypoint with orthagonal vector constrained with designated radius
+        orthagonal_x = radius * math.sin(math.atan(orthagonal_heading)) + intercept_x
+        orthagonal_y = orthagonal_heading * (orthagonal_x - intercept_x) + intercept_y
 
-            # insert avoidance WayPoint as first waypoint
-            #client.moveToPositionAsync(orthagonal_x, orthagonal_y, drone_zpos, new_speed, vehicle_name = drone).join()
-            wpl.insertWayPoint([orthagonal_x, orthagonal_y, drone_zpos], new_speed)
+        # insert avoidance WayPoint as first waypoint
+        #client.moveToPositionAsync(orthagonal_x, orthagonal_y, drone_zpos, new_speed, vehicle_name = drone).join()
+        wpl.insertWayPoint([orthagonal_x, orthagonal_y, drone[2]], new_speed)
--- SwarmScripts/CollisionDetection.py
+++ SwarmScripts/CollisionDetection.py
@@ -1,30 +1,52 @@
+from re import X
 import airsim
 import math
 import time
 import SimpleDetection
 import AvoidanceAlg
+import Vector3D
 
 class CollisionDetection:
 
     def collisionDetection(self, wpl, drone ,client):
         sd = SimpleDetection.simpleDetection()
         AA = AvoidanceAlg.AvoidanceAlg()
-        standOff = 10
+        v3D = Vector3D.Vector3D()
+        distanceArray= []
+        vectorArray = []
+        standOff = 20
+
+        droneState = client.getMultirotorState(vehicle_name = drone).kinematics_estimated
+        velocity = droneState.linear_velocity.to_numpy_array()   
+        drone_ypos = droneState.position.y_val
+        drone_xpos = droneState.position.x_val
+        drone_zpos = droneState.position.z_val
+
 
 #        while True:
-        if (sd.detectObject(client)):
+        #if (sd.detectObject(client)):
             #intiate avoidance
-            AA.rightTurnAvoid(wpl, drone, client)
-            print ("Collision Detected")
+            #AA.rightTurnAvoid(wpl, drone, client)
+            #print ("Collision Detected")
         #time.sleep(0.1)
 
             #if(sd.detectObject(client) == False):
                # break
+
         # while True:
-            # distanceArray = simpleDetection()
-            # for i in distanceArray:
-                # if distanceArray[i] < standOff:
-                    # call avoidance algorithm
-                    # print("Collision Detected")
-                    # break
+        lidarArray = sd.simpleDetection()
+        for i in lidarArray:
+            lidarPt = lidarArray[i]
+            distanceArray.append(v3D.calcDistance(lidarPt[0], lidarPt[1], lidarPt[2], drone_xpos, drone_ypos, drone_zpos))
+            # vector components from UAV to obstacle
+            objVector = v3D.vectorize(drone_xpos, drone_ypos, drone_zpos, lidarPt[0], lidarPt[1], lidarPt[2])
+            vectorArray.append(objVector)
+
+        for i in distanceArray:
+            vector = vectorArray[i]
+            # check if obstacle is within range and converging from the left, with a reduction of 5 degrees to the left.
+            if (distanceArray[i] < standOff) and (v3D.vectorAngle(vector[0], vector[1]) > 95 * math.pi / 180):
+                AA.rightTurnAvoid(wpl, [drone_xpos, drone_ypos, drone_zpos, velocity[0], velocity[1]])
+                print("Possible Collision Detected")
+                break
             # time.sleep(0.1)
