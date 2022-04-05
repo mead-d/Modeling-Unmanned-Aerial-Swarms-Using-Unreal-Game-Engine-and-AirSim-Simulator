1742efd42a6c64c73c80283e38427f9f7e8f8fea
Elijah Keck <73807523+ElijahKeck@users.noreply.github.com>
2022-04-05 13:24:41 -0400

    Add files via upload

---
+++ SwarmScripts/AnalyticData.py
@@ -0,0 +1,22 @@
+class AnalyticData:
+    #what position the drone is at
+    position = None
+
+    #what speed the drone has
+    speed = None
+
+    #what heading the drone is moving on
+    heading = None
+
+    #what point at which the avoidance manuver was initiated
+    avoidancePoint = None
+
+
+    #what avoidance rules were used
+    avoidanceRules = None
+
+    #used to repeat and identify randomized scenarios
+    seed = None
+
+    #time used for simulation
+    time = None
