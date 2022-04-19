cef759924afb429e9639df59ac50fbb042cc715a
Elijah Keck <73807523+ElijahKeck@users.noreply.github.com>
2022-04-19 15:12:57 -0400

    Add files via upload

--- SwarmScripts/CollisionAvoidanceTest.py
+++ SwarmScripts/CollisionAvoidanceTest.py
@@ -6,6 +6,7 @@ import CollisionDetection
 import WaypointList
 import SwarmPathing
 import Vector3D 
+import threading
 
 client1 = airsim.MultirotorClient()
 client2 = airsim.MultirotorClient()
@@ -17,34 +18,38 @@ d = len(client1.simListSceneObjects(name_regex= 'Drone.*')) + len(client2.simLis
 #client.confirmConnection()
 Swarming = Swarmer.Swarmer()
 cd = CollisionDetection.CollisionDetection()
-wpl = WaypointList.WaypointList()
 wpl1 = WaypointList.WaypointList()
+wpl2 = WaypointList.WaypointList()
 SwarmPathing = SwarmPathing.SwarmPathing()
 v3d = Vector3D.Vector3D()
 #d = len(client.simListSceneObjects(name_regex = 'Drone.*'))
 print("Number of drones: ", d)
-wpl.addWayPoint([-200,0,-100],4)
-wpl1.addWayPoint([200,0,-100],4)
+wpl1.addWayPoint([-200,0,-100],4)
+wpl2.addWayPoint([200,0,-100],4)
 
 
 if d == 0:
-    #client1.enableApiControl(True, "Lead")
-    #client1.armDisarm(True, "Lead")
-    #client1.takeoffAsync(vehicle_name="Lead").join()
-    #client1.moveToZAsync(-50, 10,vehicle_name="Lead").join()
 
-    client.enableApiControl(True, "Lead")
-    client.armDisarm(True, "Lead")
-    client.takeoffAsync(vehicle_name="Lead").join()
-    client.moveToZAsync(-50, 10,vehicle_name="Lead").join()
+    client1.enableApiControl(True, "Lead")
+    client1.armDisarm(True, "Lead")
+    client1.takeoffAsync(vehicle_name="Lead").join()
+    client1.moveToZAsync(-50, 10, vehicle_name="Lead").join()
+
     while True:
-        #cd.collisionDetection("Lead",wpl,client1)
-        #SwarmPathing.pathCheck(wpl,"Lead",client1)
-        #SwarmPathing.pathTo(wpl,"Lead",client1)
+        droneState = client1.getMultirotorState(vehicle_name = "Lead").kinematics_estimated
+        drone_pos = []
+        drone_pos.append(droneState.position.x_val)
+        drone_pos.append(droneState.position.y_val)
+        drone_pos.append(droneState.position.z_val)
+        drone_velocity = droneState.linear_velocity.to_numpy_array()
+        drone_pos.append(drone_velocity[0])
+        drone_pos.append(drone_velocity[1])
 
-        cd.collisionDetection("Lead",wpl,client)
-        SwarmPathing.pathCheck(wpl,"Lead",client)
-        SwarmPathing.pathTo(wpl,"Lead",client)
+        avoid_state1 = cd.collisionDetection("Lead", wpl1, client1, drone_pos)
+        if avoid_state1 is not None:
+            client1.moveToPositionAsync(avoid_state1[0], avoid_state1[1], avoid_state1[2], avoid_state1[3], vehicle_name= "Lead")
+        SwarmPathing.pathCheck(wpl1, "Lead", drone_pos)
+        SwarmPathing.pathTo(wpl1, "Lead", drone_pos)
         time.sleep(0.1)
 
     
@@ -56,53 +61,103 @@ if d == 2:
     client2.armDisarm(True, "Drone1")
 
     client1.takeoffAsync(vehicle_name="Lead")
-    client2.takeoffAsync(vehicle_name="Drone1")
+    client2.takeoffAsync(vehicle_name="Drone1").join()
 
     client1.moveToZAsync(-100, 10,vehicle_name="Lead")
-    client2.moveToZAsync(-100, 10,vehicle_name="Drone1").join()
+    client2.moveToZAsync(-102, 10,vehicle_name="Drone1").join()
 
-    #client.enableApiControl(True, "Lead")
-    #client.armDisarm(True, "Lead")
-    #client.enableApiControl(True, "Drone1")
-    #client.armDisarm(True, "Drone1")
-
-    #client.takeoffAsync(vehicle_name="Lead")
-    #client.takeoffAsync(vehicle_name="Drone1").join()
-
-    #client.moveToZAsync(-100, 10,vehicle_name="Lead")
-    #client.moveToZAsync(-100, 10,vehicle_name="Drone1").join()
     while True:
-        cd.collisionDetection("Lead",wpl,client1)
-        cd.collisionDetection("Drone1",wpl1,client2)
+
+        droneState2 = client2.getMultirotorState(vehicle_name = "Drone1").kinematics_estimated
+        drone_pos2 = []
+        drone_pos2.append(droneState2.position.x_val)
+        drone_pos2.append(droneState2.position.y_val)
+        drone_pos2.append(droneState2.position.z_val)
+        drone_velocity2 = droneState2.linear_velocity.to_numpy_array()
+        drone_pos2.append(drone_velocity2[0])
+        drone_pos2.append(drone_velocity2[1])
+
+        droneState1 = client1.getMultirotorState(vehicle_name = "Lead").kinematics_estimated
+        drone_pos1 = []
+        drone_pos1.append(droneState1.position.x_val)
+        drone_pos1.append(droneState1.position.y_val)
+        drone_pos1.append(droneState1.position.z_val)
+        drone_velocity1 = droneState1.linear_velocity.to_numpy_array()
+        drone_pos1.append(drone_velocity1[0])
+        drone_pos1.append(drone_velocity1[1])
+
+        print("Drone1 ", droneState1)
+        print("Drone2 ", droneState2)
+
+        th1 = threading.Thread(target = cd.collisionDetection,args = ["Lead", wpl1,client1,drone_pos1,"MyLidar1"])
+        th2 = threading.Thread(target = cd.collisionDetection,args = ["Lead", wpl2,client2,drone_pos2,"MyLidar2"])
+        th1.start()
+        th2.start()
+
+        avoid_state1 = th1.run()
+        #avoid_state1 = cd.collisionDetection("Lead", wpl1,client1,drone_pos1,"MyLidar1")
+        print("Drone1 ", avoid_state1)
+
+        avoid_state2 = th2.run()
+        #avoid_state2 = cd.collisionDetection("Drone1", wpl2, client2,drone_pos2,"MyLidar2")
+        print("Drone2 ", avoid_state2)
+        
+
+        
+        if ((bool(avoid_state1) is not False) and (bool(avoid_state2) is not False)):
+            print("Drones 1 and 2 avoiding")
+            f1 = client1.moveToPositionAsync(avoid_state1[0], avoid_state1[1], avoid_state1[2], avoid_state1[3], vehicle_name= "Lead")
+            f2 = client2.moveToPositionAsync(avoid_state2[0], avoid_state2[1], avoid_state2[2], avoid_state2[3], vehicle_name= "Drone1")
+            f1.join()
+            f2.join()
+        elif bool(avoid_state2) is not False:
+            print("Drone 2 avoiding")
+            f1 = client2.moveToPositionAsync(avoid_state2[0], avoid_state2[1], avoid_state2[2], avoid_state2[3], vehicle_name= "Drone1")
+            f1.join()
+        elif bool(avoid_state1) is not False:
+            print("Drone 1 avoiding")
+            f1 = client1.moveToPositionAsync(avoid_state1[0], avoid_state1[1], avoid_state1[2], avoid_state1[3], vehicle_name= "Lead")
+            f1.join()
+
         time.sleep(1)
-        SwarmPathing.pathCheck(wpl,"Lead",client1)
-        SwarmPathing.pathCheck(wpl1,"Drone1",client2)
-        SwarmPathing.pathTo(wpl,"Lead",client1,v3d)
-        SwarmPathing.pathTo(wpl1,"Drone1",client2,v3d)
-
-        #cd.collisionDetection("Lead",wpl,client)
-        #cd.collisionDetection("Drone1",wpl1,client)
-        #SwarmPathing.pathCheck(wpl,"Lead",client)
-        #SwarmPathing.pathCheck(wpl1,"Drone1",client)
-        #SwarmPathing.pathTo(wpl,"Lead",client,v3d)
-        #SwarmPathing.pathTo(wpl1,"Drone1",client,v3d)
+        SwarmPathing.pathCheck(wpl1, "Lead", client1)
+        SwarmPathing.pathCheck(wpl2, "Drone1", client2)
+        SwarmPathing.pathTo(wpl1,"Lead",client1,v3d,drone_pos1)
+        SwarmPathing.pathTo(wpl2,"Drone1",client2,v3d,drone_pos2)
         
+        th1._stop 
+        th2._stop
         
 
 
-    f1 = client.moveToZAsync(-0.1, 5,vehicle_name="Lead")
-    f2 = client.moveToZAsync(-0.1, 5,vehicle_name="Drone1")
+    f1 = client1.moveToZAsync(-0.1, 5,vehicle_name="Lead")
+    f2 = client2.moveToZAsync(-0.1, 5,vehicle_name="Drone1")
     f1.join()
     f2.join()
-    f1 = client.landAsync(vehicle_name="Lead")
-    f2 = client.landAsync(vehicle_name="Drone1")
+    f1 = client1.landAsync(vehicle_name="Lead")
+    f2 = client2.landAsync(vehicle_name="Drone1")
     f1.join()
     f2.join()
-    client.armDisarm(False, "Lead")
-    client.armDisarm(False, "Drone1")
-    client.enableApiControl(False, "Lead")
-    client.enableApiControl(False, "Drone1")
-    
+    client1.armDisarm(False, "Lead")
+    client2.armDisarm(False, "Drone1")
+    client1.enableApiControl(False, "Lead")
+    client2.enableApiControl(False, "Drone1")
+
+
+
+
+
+#
+#
+#
+# BELOW CODE DOES NOT HAVE CORRECT CLIENT1/ CLIENT2 REFERENCES!!
+#
+#
+#
+
+
+
+
 if d == 20:
     client.enableApiControl(True, "Lead")
     client.enableApiControl(True, "Drone1")
