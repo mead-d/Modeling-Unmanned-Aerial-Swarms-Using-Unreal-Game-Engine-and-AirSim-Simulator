157d8fce83ec2003471cfb05311237cdf2e2612d
mead-d <meadd@my.erau.edu>
2022-04-21 19:49:06 -0400

    Fixes formatting problem

--- SwarmScripts/AnalyticData.py
+++ SwarmScripts/AnalyticData.py
@@ -1,116 +1,100 @@
 class AnalyticData:
-    
-    #Test main
-	#def __init__(self):
-	    #self._position = 0;
-	    #self._speed = 0;
-	    #self._heading = 0;
-	    #self._avoidancePoint = 0;
-	    #self._objDist = 0;
-	    #self._objHeading = 0;
-	    #self._sensorRange = 0;
-	    #self._standoffRange = 0;
 
-    
-    #what position the drone is at
-	
+    #Initialize data variables
+    def __init__(self):
+        self._position = []
+        self._speed = []
+        self._heading = []
+        self._avoidancePoint = []
+        self._objDistance = []
+        self._objVector = []
+        self._sensorRange = []
+        self._avoidanceRange = []
+        self._rule = []
+        self._seed = []
+        self._simTime = []
+        self._avoidanceTime = []
+
+    #drone position
     def get_position(self):
         return self._position
 
     def set_position(self, x):
-	    self._position = x
-	
+        self._position = x
 
-    #what speed the drone has
-
+    #drone speed
     def get_speed(self):
-       return self._speed
+        return self._speed
 
     def set_speed(self, x):
-       self._speed = x
+        self._speed = x
 
-    #what heading the drone is moving on
-     
+    #drone heading
     def get_heading(self):
-       return self._heading
+        return self._heading
 
     def set_heading(self, x):
-	   self._heading = x
+        self._heading = x
 
-    #what point at which the avoidance manuver was initiated
-    
+    #coordinate of avoidance start
     def get_avoidancePoint(self):
-       return self._avoidancePoint
-    
+        return self._avoidancePoint
+
     def set_avoidancePoint(self, x):
-	   self._avoidancePoint = x
+        self._avoidancePoint = x
 
-    #detected obstacle distance from drone POV
-  
-
+    #distance of avoided obstacle from vehicle
     def get_objDist(self):
-      return self.objDist
+        return self._objDistance
 
     def set_objDist(self, x):
-	  self._objDist = x
+        self._objDist = x
+    
+    #Obstacle vector from vehicle
+    def get_objVector(self):
+        return self._objVector
 
-    #obstacle heading from the drone to the obstacle
+    def set_objVector(self, x):
+        self._objVector = x
 
-	def get_objHeading(self):
-      return self._objHeading
-
-    def set_objHeading(self, x):
-	  self._objHeading = x
-
-    #sensor Range
-
+    #Sensor range
     def get_sensorRange(self):
-      return self._sensorRange
+        return self._sensorRange
 
     def set_sensorRange(self, x):
-	  self._sensorRange = x
+        self._sensorRange = x
 
-    #Standoff range
+    #avoidance range
+    def get_avoidanceRange(self):
+        return self._avoidanceRange
 
-    def get_standoffRange(self):
-      return self._standoffRange
+    def set_avoidanceRange(self, x):
+        self._avoidanceRange = x
 
-    def set_standoffRange(self, x):
-	  self._standoffRange = x
+    #avoidance rule (behavior)
+    def get_rule(self):
+        return self._rule
 
-
-
-
-    #what avoidance rules were used
-    def get_rules(self):
-      return self._rules
-
-    def set_rules(self, x):
-	  self._rules = x
-
-
-    #used to repeat and identify randomized scenarios
+    def set_rule(self, x):
+        self._rule = x
     
+    #Seed of scenario
     def get_seed(self):
-      return self._seed
+        return self._speed
 
     def set_seed(self, x):
-	  self._seed = x
+        self._seed = x
 
-
-    #time used for simulation
-    
+    #scenario execution time
     def get_simTime(self):
-      return self._simTime
+        return self._simTime
 
     def set_simTime(self, x):
-	  self._simTime = x
+        self._simTime = x
 
-
-    #time used for avoidance
-    
+    #completion time for avoidance maneuver
     def get_avoidanceTime(self):
-      return self._avoidanceTime
+        return self._avoidanceTime
 
     def set_avoidanceTime(self, x):
-	  self._advoidanceTime= x
+        self._avoidanceTime = x
